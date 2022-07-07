fae2ee3cad58db7dc4ac2a57274d7dd16b3b0f7c
edward795 <ayyappadasgb777999@gmail.com>
2022-07-07 09:39:14 +0530

    #usecase 1: Accordion

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Accordion.css";
+
+export const Accordion = () => {
+  return (
+    <div className="parentAccord">
+      <div className="accord"></div>
+      <br></br>
+      <div className="childAccord"></div>
+    </div>
+  );
+};
