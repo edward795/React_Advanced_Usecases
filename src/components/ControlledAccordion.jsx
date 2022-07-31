5766c2d26d0040133d94bd8984724e4fde4c094a
edward795 <ayyappadasgb777999@gmail.com>
2022-07-31 09:42:06 +0530

    #usecase : controlled Accordion

---
+++ src/components/ControlledAccordion.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function ControlledAccordions({ content, index }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        expanded={expanded === `panel${index}`}
+        onChange={handleChange(`panel${index}`)}
+        sx={{ backgroundColor: "#be3551" }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ color: "text.secondary" }}>
+            accordion {index}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{content}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
